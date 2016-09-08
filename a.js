09e789d88d8f21abbfacceba045657211d5e28be
LeeJangHun <jjanghuni102@naver.com>
2016-09-08 19:47:22 +0900

    data

---
+++ a.js
@@ -0,0 +1,12 @@
+
+var image = new Image()
+image.src = "back.jpg"
+
+window.addEventListener('load',onLoad);
+
+function onLoad(){
+    var canvas = document.getElementById('game_canvas')
+    var context = canvas.getContext('2d')
+
+
+}
