ad3711b65ea49f3d8b2d681e6b8713534d147e5f
LeeJangHun <jjanghuni102@naver.com>
2016-09-08 20:44:05 +0900

    Sec-day key check

--- a.js
+++ a.js
@@ -1,12 +1,38 @@
 
 var image = new Image()
+var eventType, keyCode
 image.src = "back.jpg"
 
-window.addEventListener('load',onLoad);
+image.addEventListener('load',onLoad);
+
+window.addEventListener('keydown',onKeyDown);
+window.addEventListener('keyup',onKeyUp)
 
 function onLoad(){
     var canvas = document.getElementById('game_canvas')
     var context = canvas.getContext('2d')
 
+    canvas.width = 500;
+    canvas.height = 500;
 
+    context.fillStyle = '#666'
+    context.font = '15px Arial'
+    context.textBaseline = 'top'
+    context.fillText('KeyCode is : '+keyCode,10,10)
+    context.fillText('eventType is : '+eventType, 10,25)
+
+    context.drawImage(image,100,100)
+
+}
+
+function onKeyDown(e){
+  keyCode = e.keyCode
+  eventType = e.type
+  onLoad()
+}
+
+function onKeyUp(e){
+  keyCode = e.keyCode
+  eventType = e.type
+  onLoad()
 }
